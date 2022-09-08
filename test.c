d3241440d7fb97c61d92539983517af7e6cae849
Aziz Harkati <azizharkati20@gmail.com>
2022-09-08 14:41:06 +0100

    updating

--- test.c
+++ test.c
@@ -2,44 +2,62 @@
 
 int main() {
 	
+	// Simple Bank System
+	const int pw = 1234;
 	float balance = 150.36;
-	float add;
-	float withdraw;
+	float add; 
+	float withdraw; 
 	int password;
 	int operation;
+	int shot = 0;
 	
 	
-	printf("^^^^ Welcome To Your Favourite Bank ^^^^ \n\n");
-	
-	printf("--- Enter Your Password --- \n");
-	scanf("%d" , &password);
-	
-	if(password  = 1234) {
-		printf("\nWelcome To Your Account \n\n");
-		printf("1 - ADD ******\n2 - WITHDRAW ******\n\n");
-		printf("Choose Your Operation ( Type 1 To Add and 2 To WITHDRAW)\n");
-		scanf("%d" , &operation);
-		switch(operation) {
-			case 1:
-				printf("Add Something To Your Balance \n");
-				scanf("%f" , &add);
-				balance += add;
-				break;
-			case 2:
-				printf("\nEnter a Price To Withdraw\n");
-				scanf("%f" , &withdraw);
-				if(withdraw > balance) {
-					
-					printf("\nYou Don't Have This in Your Balance \n");
-				} else {
-					balance -= withdraw;
-				}
-		}
+	    printf("^^^^ Welcome To Your Favourite Bank ^^^^ \n\n");
+		printf("--- Enter Your Password --- \n");
+	    scanf("%d" , &password);
+	    
+	    // Password Checkout
+	    while(password  != 1234 && shot < 2) {
+		    
+		    printf("Wrong Password !\n");
+		    printf("\n--- Re-enter Your Password --- \n");
+	        scanf("%d" , &password);
 		
-	} else {
-		printf("Wrong Password \n");
-	}
-	
-	printf("\nYour Balance Now is %.2f $\n" , balance);
+	   }
+	   if(shot == 2) {
+	   	     
+	   	    printf("Your Card Has Been Taken Go To Your Bank Area To Get it Back  \n");
+	   }
+	   else {
+	   	  
+	   	  printf("\nWelcome To Your Account \n\n");
+		    printf("1 - ADD ******\n2 - WITHDRAW ******\n\n");
+		    printf("Choose Your Operation ( Type 1 To Add and 2 To WITHDRAW)\n");
+		    scanf("%d" , &operation);
+		    switch(operation) {
+			    case 1:
+				   printf("Add Something To Your Balance \n");
+				   scanf("%f" , &add);
+				   balance += add;
+				   printf("\nYou Added %.2f $\n" , add);
+				   printf("\nYour Balance Now is %.2f $\n" , balance);
+				   break;
+			    case 2:
+				   printf("\nEnter a Price To Withdraw\n");
+				   scanf("%f" , &withdraw);
+				   
+				   // Balance Checkout
+				   if(withdraw > balance) {
+					
+					    printf("\nYou Don't Have This in Your Balance \n");
+					    printf("\nYour Balance Now is %.2f $\n" , balance);
+
+				  } else {
+					 balance -= withdraw;
+					 printf("\nYour Balance Now is %.2f $\n" , balance);
+
+				  }
+		   }  
+	   }
 	return 0;
 }
