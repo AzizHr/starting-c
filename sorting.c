cdd026ebe65f763d6bb812fd3d451d5a8ef4f501
Aziz Harkati <azizharkati20@gmail.com>
2022-09-15 14:47:24 +0100

    sorting_methodes_in_c

--- sorting.c
+++ sorting.c
@@ -17,41 +17,41 @@ int main(){
             printf("| %d |" , arr[i]);
         }
         // insertion sort
-        // int arr[5] = {5,6,2,9,1} , i , help;
-        // int con;
-        // do{
-        //     con = 0;
-        //     for(i = 1 ; i < 5 ; i++){
-        //         if(arr[i] < arr[i-1]){
-        //             con = 1;
-        //             help = arr[i-1];
-        //             arr[i-1] = arr[i];
-        //             arr[i] = help;
-        //         }
-        //     }
-        // }while(con == 1);
+        int arr[5] = {5,6,2,9,1} , i , help;
+        int con;
+        do{
+            con = 0;
+            for(i = 1 ; i < 5 ; i++){
+                if(arr[i] < arr[i-1]){
+                    con = 1;
+                    help = arr[i-1];
+                    arr[i-1] = arr[i];
+                    arr[i] = help;
+                }
+            }
+        }while(con == 1);
 
-        // for(i = 0 ; i < 5 ; i++){
-        //     printf("| %d |" , arr[i]);
-        // }
+        for(i = 0 ; i < 5 ; i++){
+            printf("| %d |" , arr[i]);
+        }
 
     // bubble sort 
-    // int arr[5] = {5,6,2,9,1} , i , help;
-    // int con;
-    // do{
-    //     con = 0;
-    //     for(i = 0 ; i < 4 ; i++){
-    //         if(arr[i] > arr[i+1]){
-    //             con = 1;
-    //             help = arr[i];
-    //             arr[i] = arr[i+1];
-    //             arr[i+1] = help;
-    //         }
-    //     }
-    // }while(con == 1);
+    int arr[5] = {5,6,2,9,1} , i , help;
+    int con;
+    do{
+        con = 0;
+        for(i = 0 ; i < 4 ; i++){
+            if(arr[i] > arr[i+1]){
+                con = 1;
+                help = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = help;
+            }
+        }
+    }while(con == 1);
 
-    // for(i = 0 ; i < 5 ; i++){
-    //     printf("| %d |" , arr[i]);
-    // }
+    for(i = 0 ; i < 5 ; i++){
+        printf("| %d |" , arr[i]);
+    }
     return 0;
 }
